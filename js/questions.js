151f221389d8446aacbb6c00b22c1cd0fdb8789d
Edson Aguiar <ea03791.com>
2023-02-27 20:29:57 +0000

    Scholar Quiz

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+//creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "What is the Capital of the U.S.A?",
+        answer:"Washington, D.C",
+        options: [
+            "New York",
+            "Washington, D.C",
+            "Los Angeles",
+            "Chicago"
+        ]
+    },
+    {
+       numb: 2,
+       question: "Where is the eiffel tower located?",
+       answer: "France",
+       options: [
+           "Germany",
+           "China",
+           "Portugal",
+           "France"
+       ]
+   },
+   {
+       numb: 3,
+       question: "In which country was Leonardo da Vinci born?",
+       answer: "Italy",
+       options: [
+           "Italy",
+           "China",
+           "Portugal",
+           "Russia"
+       ]
+   },
+   {
+       numb: 4,
+       question: "What is the longest river in the world?",
+       answer: "Nilo",
+       options: [
+           "Mississippi",
+           "Yangtzé",
+           "Amazonas",
+           "Nilo"
+       ]
+   },
+   {
+       numb: 5,
+       question: "What is the safest country in the world?",
+       answer: "Iceland",
+       options: [
+           "Iceland",
+           "Japan",
+           "Norway",
+           "Portugal"
+       ]
+   },
+
+];
